eb89886df3aa5a7004e1f6ffe30bf44870d36bc4
pandesalNaSunog <floresjem8@gmail.com>
2022-04-29 14:20:24 +0800

    latest

--- admin/deleteService.php
+++ admin/deleteService.php
@@ -1,15 +1,13 @@
 <?php
 
-    // include('connection.php');
-    // $con = connect();
-    // if(isset($_POST)){
-    //     $serviceId = $_POST['service_id'];
+    include('connection.php');
+    $con = connect();
+    if(isset($_POST)){
+        $serviceId = $_POST['service_id'];
 
-    //     $query = "DELETE FROM services WHERE id = '$serviceId'";
-    //     $con->query($query) or die($con->error);
+        $query = "DELETE FROM services WHERE id = '$serviceId'";
+        $con->query($query) or die($con->error);
 
-    //     echo 'deleted';
-    // }
-
-    echo $_POST['service_id'];
+        echo 'deleted';
+    }
 ?>
