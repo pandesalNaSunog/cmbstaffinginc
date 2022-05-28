6661bf2021c8b70224b1377fea188a81636878a6
pandesalNaSunog <floresjem8@gmail.com>
2022-05-28 10:24:59 +0800

    latest

--- getServices.php
+++ getServices.php
@@ -5,9 +5,8 @@
     $query = "SELECT * FROM services LIMIT 4";
     $services = $con->query($query) or die($con->error);
     $data = array();
-    while ($row = $services->fetch_assoc()) {
+    while ($row = $services->fetch_assoc()){
         $data[] = $row;
     }
-
     echo json_encode($data);
 ?>
