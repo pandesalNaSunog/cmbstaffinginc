18cda541f870ee7ba1c3452dc02f166b533e2849
pandesalNaSunog <floresjem8@gmail.com>
2022-05-28 18:43:06 +0800

    latest

--- cmbstaffingapi/routes/api.php
+++ cmbstaffingapi/routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/sample', [SampleController::class, 'sample']);
+Route::post('/sample', [SampleController::class, 'sample']);
